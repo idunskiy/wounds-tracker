1bb027b923e1afc184497527b4bcc12f4210130c
ivan <i.dunskiy@gmail.com>
2025-02-28 14:48:19 +0400

    After tables created

--- app.py
+++ app.py
@@ -193,9 +193,9 @@ with app.app_context():
     initialize_retriever()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        print("Database tables created successfully.")
+    # with app.app_context():
+    #     db.create_all()
+    #     print("Database tables created successfully.")
 
     if os.getenv('DEBUG', 'False').lower() not in ['true', '1', 'yes']:  # Production mode
         app.run(
